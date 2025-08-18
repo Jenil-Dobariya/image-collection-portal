c8b9d7e13e1b573fbf8baa292e6ccb72dc7608ba
Jenil Dobariya <jenildobariya@Jenils-MacBook-Pro.local>
2025-08-18 16:19:11 +0530

    upadte gmail auth for nodemailer

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -12,9 +12,7 @@ const saltRounds = 10;
 
 // --- Nodemailer Setup ---
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false, // true for 465, false for other ports
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
